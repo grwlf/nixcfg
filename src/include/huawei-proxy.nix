c036e841c6a0078d76ef0353f131d31c5f882ea3
Sergey Mironov <grrwlf@gmail.com>
2019-04-03 17:06:10 +0300

    mrc-cbg-2: add the configuration

---
+++ src/include/huawei-proxy.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, ... } :
+let
+  port = "3128";
+
+  cntlm_ini = pkgs.writeText "cntlm.ini" ''
+    Username    mwx579795
+    Domain      CHINA
+    Proxy       proxyru.huawei.com:8080
+    Auth        NTLM
+    PassLM      7B4AD4F417B80BCCD4169DD66E4838D5
+    PassNT      6127180564B5CF450875219084A134BC
+    PassNTLMv2  A363AB844E34D374406B62427D472919    # Only for user 'mWX579795', domain 'CHINA'
+    NoProxy     localhost,127.0.0.*,*.huawei.com,*.fi-git-rd.huawei.com,10.122.225.21,10.175.100.97,10.175.100.76,internal.domain
+  '';
+
+  huawei-proxy = pkgs.writeShellScriptBin "huawei-proxy" ''
+    ${pkgs.cntlm}/bin/cntlm -c ${cntlm_ini} -l 0.0.0.0:${port} -f
+  '';
+in
+{
+  security.pki.certificateFiles = [
+    "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+    ./../certs/huawei.crt
+  ];
+
+  networking.proxy.default = "http://127.0.0.1:${port}";
+  networking.proxy.noProxy = "fi-git-rd.huawei.com,10.122.225.21,10.175.100.97,10.175.100.76,127.0.0.1,localhost,internal.domain";
+
+  systemd.services.huawei-proxy = {
+    description = "Huawei proxy service";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = ''${huawei-proxy}/bin/huawei-proxy'';
+      # ExecStop = ''${pkgs.procps}/bin/pkill cntlm'';
+      Restart = "on-failure";
+    };
+  };
+
+  #systemd.services.huawei-proxy.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    cntlm
+    huawei-proxy
+  ];
+}
+
---
+++ src/mrc-cbg-2.nix
@@ -0,0 +1,195 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
+    ];
+
+  require = [
+      ./include/subpixel.nix
+      # ./include/bashrc.nix
+      # ./include/cvimrc.nix
+      ./include/systools.nix
+      ./include/templatecfg.nix
+      ./include/wheel.nix
+      ./include/ntpd.nix
+      ./include/huawei-proxy.nix
+      # ./include/containers.nix FIXME
+    ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/eb60af14-352d-4931-b453-114bddcf255c";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/04FD-50B1";
+      fsType = "vfat";
+    };
+
+  fileSystems."/home" =
+    { device = "/dev/disk/by-label/HOME";
+      fsType = "ext4";
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/0a1855f0-ed0f-4847-a6f5-02245ef7e28a"; }
+    ];
+
+  nix.maxJobs = lib.mkDefault 28;
+  powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
+
+  boot.loader.systemd-boot.enable = true;
+
+  networking.networkmanager.enable = false;
+  networking.hostName = "mrc-cbg-02";
+  networking.defaultGateway.address = "10.199.192.129";
+  networking.dhcpcd.enable = false;
+  networking.firewall.enable = false;
+  networking.interfaces."enp0s31f6".ipv4.addresses = [
+    { address = "10.199.192.149"; prefixLength = 25; }
+  ];
+  networking.nameservers = [
+    "10.129.29.84"
+    "10.129.29.85"
+  ];
+
+  i18n = {
+    defaultLocale = "ru_RU.UTF-8";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Moscow";
+
+  services.openssh = {
+    enable = true;
+    ports = [22];
+    permitRootLogin = "yes";
+    extraConfig = ''
+      ClientAliveInterval 20
+    '';
+  };
+
+  services.xserver = {
+    enable = true;
+
+    videoDrivers = [ "nvidia" ];
+
+    layout = "us,ru";
+
+    #xkbOptions = "grp:alt_space_toggle, ctrl:swapcaps, grp_led:caps";
+
+    desktopManager = {
+      xfce.enable = true;
+    };
+
+    displayManager = {
+      lightdm.enable = true;
+      lightdm.extraSeatDefaults = "xserver-allow-tcp=true";
+      lightdm.extraConfig = ''
+      [XDMCPServer]
+      enabled=true
+      port=177
+      '';
+      xserverArgs = [ "-listen tcp" ];
+    };
+
+    serverFlagsSection = ''
+      Option "BlankTime" "0"
+      Option "StandbyTime" "0"
+      Option "SuspendTime" "0"
+      Option "OffTime" "0"
+    '';
+
+    enableTCP = true;
+  };
+
+
+  users.extraUsers =
+    let
+      mkuser = name : id : {
+        "${name}" = {
+          uid = id;
+          isNormalUser = true;
+          extraGroups = ["docker"];
+          initialPassword = "huawei123";
+        };
+      };
+    in
+    (mkuser "mironov" 1048) //
+    (mkuser "grechanik" 1050) //
+    (mkuser "khalikov" 1070) //
+    (mkuser "romanov" 1071) //
+    (mkuser "murygin" 1072);
+
+  hardware = {
+    opengl.driSupport32Bit = true;
+    enableAllFirmware = true;
+  };
+
+  virtualisation.docker = {
+    enable = true;
+    enableNvidia = true;
+  };
+
+  environment.extraInit = ''
+    export NIXCFG_ROOT=/home/nixcfg
+    export NIX_PATH="nixpkgs=$NIXCFG_ROOT/nixpkgs:nixos=$NIXCFG_ROOT/nixpkgs/nixos:nixos-config=$NIXCFG_ROOT/src/mrc-cbg-2.nix"
+  '';
+
+  environment.shellAliases = {
+    nix-env = "nix-env -f '<nixpkgs>'";
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    unclutter
+    xorg.xdpyinfo
+    xorg.xinput
+    (rxvt_unicode-with-plugins.override {
+      plugins = [
+        urxvt_perl
+        urxvt_theme_switch
+      ];
+    })
+    glxinfo
+    networkmanagerapplet
+    chromium
+    plowshare
+    lsof
+    evince
+    vim
+  ];
+
+
+  # Enable touchpad support.
+  # services.xserver.libinput.enable = true;
+
+  # Enable the KDE Desktop Environment.
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.users.guest = {
+  #   isNormalUser = true;
+  #   uid = 1000;
+  # };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  #system.stateVersion = "18.09"; # Did you read the comment?
+
+  nixpkgs.config.allowUnfree = true;
+
+}
