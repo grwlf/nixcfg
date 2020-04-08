9209d2bf873763e9fba290016cc01ec840392994
Sergey Mironov <grrwlf@gmail.com>
2020-04-08 17:21:37 +0300

    lssh: add -Y flag

--- src/pkgs/lssh.nix
+++ src/pkgs/lssh.nix
@@ -10,7 +10,7 @@ shift
 IF=`ssh "$T" route -n | tail -n 1 | awk '{print $(NF)}'`
 IP=`ssh "$T" ip -4 -o addr show $IF | awk '{print $4}' | sed 's/\/.*//'`
 
-ssh "$@"  "$IP"
+ssh -Y "$@"  "$IP"
 ''
 
 
