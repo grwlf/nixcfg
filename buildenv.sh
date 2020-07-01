b3036be45ebd1f95a714bf3d4db50fba69c96ef1
Sergey Mironov <grrwlf@gmail.com>
2020-07-01 18:34:41 +0300

    Update build-env, support galtimir hacks

--- buildenv.sh
+++ buildenv.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-if test -n "$1" ; then
-  SUFFIX="-$1"
+if test "$USER" != "grwlf" ; then
+  SUFFIX="-$USER"
 else
   SUFFIX=""
 fi
 
-nix-build src/pkgs/myenv${SUFFIX}.nix --argstr me $USER && nix-env -i ./result
+/var/run/current-system/sw/bin/nix-build src/pkgs/myenv${SUFFIX}.nix --argstr me $USER -o /tmp/result-env  && \
+/var/run/current-system/sw/bin/nix-env -i /tmp/result-env
