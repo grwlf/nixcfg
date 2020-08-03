0a74f3c6f94db3a3999b99e5ef7daa6192e4fbcd
Sergey Mironov <grrwlf@gmail.com>
2020-08-03 11:57:18 +0300

    buildenv.sh: add -K to keep temp dirs

--- buildenv.sh
+++ buildenv.sh
@@ -6,5 +6,8 @@ else
   SUFFIX=""
 fi
 
-/var/run/current-system/sw/bin/nix-build src/pkgs/myenv${SUFFIX}.nix --argstr me $USER -o /tmp/result-env  && \
+/var/run/current-system/sw/bin/nix-build src/pkgs/myenv${SUFFIX}.nix \
+  --argstr me $USER \
+  -o /tmp/result-env  \
+  -K && \
 /var/run/current-system/sw/bin/nix-env -i /tmp/result-env
